149484fb6c1652062670fd94989991d73ac7bd1a
syq1035 <1035394794@qq.com>
2019-07-16 09:46:34 +0800

    modify search page

---
+++ src/services/search/index.ts
@@ -0,0 +1,19 @@
+import { Service } from '..'
+import { action } from 'mobx'
+
+export class SearchService extends Service {
+
+  constructor (path: string = '/api/query') {
+    super(path)
+  }
+
+  @action public async exportExcel (data: any = {}): Promise<any> {
+    return this.post('/export', data)
+  }
+
+  @action public async getList (data: any = {}): Promise<any> {
+    return this.get('/list', data)
+  }
+}
+
+export default new SearchService()
